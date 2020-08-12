ef6d5cb575a38f4cf0ade7a9aa29a1d4306700d2
Paola <ypaolaporrasrueda@gmail.com>
2020-08-12 00:10:46 -0500

    resolve

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const factorial = (number) => {
+    if (number < 0) return -1;
+    else if (number == 0) return 1;
+    else {
+      return number * factorial(number - 1);
+    }
+  };
+  module.exports = factorial;
